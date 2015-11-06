ca67aa824750e7c7470eb23b0681cf2067f0e2c1
Reuben Osborne <reuben.osborne@44391-501.auckland.natcoll.ac.nz>
2015-11-06 19:32:52 +1300

    Successful angular yay

---
+++ public/app/controllers/mainController.js
@@ -0,0 +1,5 @@
+angular.module('app.controllers')
+
+.controller('mainController', function($scope) {
+	$scope.success = 'Yes! You did it!'
+})
--- resources/views/index.php
+++ resources/views/index.php
@@ -7,7 +7,7 @@
 	<title>Contact</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 	<link rel="stylesheet" href="assets/css/style.min.css">
-	<script src="components/angular/angular.min.js"></script>
+
 </head>
 
 
@@ -18,9 +18,7 @@
 
 
 
-<script src="bower_components/angular-resource/angular-resource.min.js"></script>
-<script src="bower_components/angular-sanitize/angular-sanitize.min.js"></script>
-<script src="bower_components/lodash/lodash.min.js"></script>
-<script src="assets/js/main.js"></script>
+<script src="dist/libs.min.js"></script>
+<script src="dist/app.min.js"></script>
 </body>
 </html>
