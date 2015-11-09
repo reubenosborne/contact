95a1c0e70f98c29e272b81f9ecc126f189579e44
Reuben Osborne <reuben.osborne@gmail.com>
2015-11-10 12:39:49 +1300

    Plenty of styling

--- public/app/controllers/mainController.js
+++ public/app/controllers/mainController.js
@@ -26,6 +26,10 @@ angular.module('app.controllers')
 
 		var contact = new Contact(); // Create new contact
 
+		$scope.selectedContact = contact;
+
+		$scope.editMode = true;
+
 		contact.name = $scope.newContactName; // Set name to the input box content
 
 		// Make a callback to handle what happens when the server responds
@@ -61,15 +65,19 @@ angular.module('app.controllers')
 
 	$scope.deleteContact = function() {
 
-		$scope.selectedContact.$delete(function() {
+		var result = confirm("Are you sure you want to delete this contact?");
+		
+		if (result) {
 
-			$scope.allContacts = _.reject($scope.allContacts, function(contact) {
+			$scope.selectedContact.$delete(function() {
+
+				$scope.allContacts = _.reject($scope.allContacts, function(contact) {
 				return contact.id === $scope.selectedContact.id;
+				})
+
+				$scope.selectedContact = undefined;
 			})
-
-			$scope.selectedContact = undefined;
-
-		});
+		}
 	};
 
 })
--- resources/views/index.php
+++ resources/views/index.php
@@ -1,21 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
 
-
 <head>
 	<meta charset="UTF-8">
 	<title>Contact</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-	<link rel="stylesheet" href="dist/style.min.css">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="dist/style.min.css">
 </head>
-
 
 <body>
 
-
 	<div ng-view></div>
-
 
 
 <script src="dist/libs.min.js"></script>
