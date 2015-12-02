de39df8f22bcc25616f658916f6c7a5f495e3de3
Reuben Osborne <reuben.osborne@44391-501.auckland.natcoll.ac.nz>
2015-12-02 19:45:39 +1300

    Added file deletion. Removed contact delete.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -17,7 +17,6 @@ use App\File;
 
 
 
-
 // Home page
 Route::get('/', ['middleware' => 'auth', function() {
 	return view('index');
@@ -26,9 +25,9 @@ Route::get('/', ['middleware' => 'auth', function() {
 // Authentication routes
 Route::get('auth/login', 'Auth\AuthController@getLogin');
 Route::post('auth/login', 'Auth\AuthController@postLogin');
-Route::get('auth/logout', 'Auth\AuthController@getLogout');
+Route::get('/logout', 'Auth\AuthController@getLogout');
 
-// Registration routes
+// Registration routes - Delete when ready
 Route::get('auth/register', 'Auth\AuthController@getRegister');
 Route::post('auth/register', 'Auth\AuthController@postRegister');
 
