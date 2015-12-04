a5e4e0c3593ff89f0978f6c3572ae986bc31173d
Reuben Osborne <reuben.osborne@44391-501.auckland.natcoll.ac.nz>
2015-12-04 17:22:27 +1300

    Code review and clean up

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -4,7 +4,7 @@
 	<meta charset="UTF-8">
 	<title>Contacts</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-	<link rel="stylesheet" href="">
+	<link rel="stylesheet" href="/public/dist/style.min.css">
 </head>
 <body>
 
@@ -15,6 +15,5 @@
 	</div>
 
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0-alpha1/jquery.min.js"></script>
-	<script src="/js/laravel-delete.js"></script>
 </body>
 </html>
